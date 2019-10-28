296717e506cc22212ff20135f01ad7fa9bc99825
alexanderaliferis <alexander.daliferis@gmail.com>
2019-10-28 23:17:59 +0100

    Neural Net works?

--- functions.py
+++ functions.py
@@ -1,17 +1,53 @@
 import pandas as pd
 import numpy as np
 from sklearn import datasets
+from sklearn.model_selection import train_test_split
 import random
+import seaborn as sns
 import sys
 import matplotlib.pyplot as plt
 random.seed(209)
+np.random.seed(100)
 
 from sklearn.linear_model import LogisticRegression
 
+
 """
-Functions used to generate data for testing
+--------------------------------------------------------------------
+Class test function
+--------------------------------------------------------------------
 """
 
+def grid_search(class_input,sizes,X_test,y_test,etas,lamdbdas):
+    accuracies = np.zeros((len(etas),len(lamdbdas)))
+    sns.set()
+
+    for i,eta_in in enumerate(etas):
+        for j,lamb in enumerate(lamdbdas):
+            print("Eta",eta_in)
+            #NN = NeuralNetwork(X_train,y_train,sizes,epochs=100, batch_size=50,eta=eta_in)
+            Object = class_input(X_train,y_train,sizes,epochs=100, batch_size=50,eta=eta_in,lmbd=lamb)
+            probabilities = Object.predict(X_test,classify=True)
+            #print("Probabilities", probabilities)
+            #print("Fasit",y)
+            accuracies[i][j] = accuracy_score_numpy(probabilities,y_test)
+            #print("Accuracy",accuracies[i])
+
+
+    print("Accuracies",accuracies)
+    fig, ax = plt.subplots(figsize=(20, 10))
+    sns.heatmap(accuracies, annot=True, ax=ax, cmap="viridis")
+    ax.set_title("Accuracy")
+    ax.set_ylabel("$\eta$")
+    ax.set_xlabel("$\lambda$")
+    plt.show()
+
+
+"""
+--------------------------------------------------------------------
+Functions used to generate data for testing
+--------------------------------------------------------------------
+"""
 
 #Generate data for testing
 def generate_data():
@@ -23,9 +59,28 @@ def load_data(path, header):
     marks_df = pd.read_csv(path, header=header)
     return marks_df
 
+""" 
+--------------------------------------------------------------------
+General functions
+--------------------------------------------------------------------
+"""
+
+def sigmoid(x):
+    return 1/(1 + np.exp(-x))
+
+
+def sigmoid_der(x):
+    return sigmoid(x) * (1-sigmoid(x))
+
+
+def accuracy_score_numpy(Y_test, Y_pred):
+    return np.sum(Y_test == Y_pred) / len(Y_test)
+
 
 """
+--------------------------------------------------------------------
 Classes used
+--------------------------------------------------------------------
 
 """
 
@@ -148,7 +203,7 @@ class logit:
                 self.weights -= eta*self.gd(X,y,betas_old)
             elif iter == 'SGD':
                 print("Batch_size",n)
-                self.SGD(X,y,2000,int(len(X[:])/10),eta_fixed=True)
+                self.SGD(X,y,100,int(len(X[:])/100),eta_fixed=True)
                 break
 
         return self.weights
@@ -157,13 +212,6 @@ class logit:
     #Need to create predict
     #def fit(self,X_train,y_train):
 
-
-def sigmoid(x):
-    return 1/(1 + np.exp(-x))
-
-
-def sigmoid_der(x):
-    return x * (1-x)
 
 
 """
@@ -175,7 +223,7 @@ That when called within another object, gives inheritance to the attributes
 Use classes and @staticmethod to avoid many different functions and easy the change of cost funtion 
 within the Neural Net class
 
-Note that the scaling is simplified as this does not change the algebraic behaviour to finf minima
+Note that the scaling is simplified as this does not change the algebraic behaviour to find minima
 
 """
 class CrossE_Cost:
@@ -207,16 +255,16 @@ class NeuralNetwork:
     #Tune lambda for Regularization
     def __init__(
             self,
-            X_data,
-            Y_data,
-            cost = CrossE_Cost,
+            X,
+            y,
             sizes,
+            cost=CrossE_Cost,
             epochs=10,
             batch_size=100,
-            #eta=0.1,
-            lmbd=0.0):
-        self.X_data_full = X_data
-        self.Y_data_full = Y_data
+            eta=0.1,
+            lmbd=0.000):
+        self.X = X
+        self.y = y
         self.sizes = sizes
         self.cost = cost
         #self.n_inputs = X_data.shape[0]
@@ -229,16 +277,21 @@ class NeuralNetwork:
         #self.eta = eta
         self.lmbd = lmbd
         self.n_layers = len(sizes)
-        self.weights = self.create_biases_and_weights()
+        self.create_biases_and_weights()
+        self.SGD(X,y,epochs,batch_size,eta)
 
     def create_biases_and_weights(self,zeros=False):
         #self.weights = np.random.randn(self.n_features, self.n_hidden_neurons)
         #self.biases = np.zeros(self.n_hidden_neurons) + 0.01
-        if zeros:self.biases = [np.zeros(y, 1) for y in range(self.sizes[1:])]
-        else:self.biases = [np.random.randn(y, 1) for y in range(self.sizes[1:])]
+
+        if zeros:self.biases = [np.zeros(y, 1) for y in self.sizes[1:]]
+        else:self.biases = [np.random.randn(y, 1) for y in self.sizes[1:]]
+
         #Note x & y changed to use np.dot instead of matmul
         #Connections between prev layer with X number of neurons and next layer of neurons
-        self.weights = [np.random.randn(y, x) for x, y in zip(self.sizes[:1], self.sizes[:-1])]
+
+        self.weights = [0.001*np.random.randn(y, x) for x, y in zip(self.sizes[:-1], self.sizes[1:])]
+        #print(self.weights)
 
 
     #Simple feed-forward algorithm
@@ -254,20 +307,24 @@ class NeuralNetwork:
         --> out for first of arrays these
         """
         #Initialize array
-        der_w = [np.zeros(bias.shape) for bias in self.biases]
-        der_b = [np.zeros(weight.shape) for weight in self.weights]
+        der_b = [np.zeros(bias.shape) for bias in self.biases]
+        der_w = [np.zeros(weight.shape) for weight in self.weights]
         #Initialize activation arrays
-        activation = x
+        X = X.reshape(-1,1)
+        activation = X
         #Store all activations and zs regardless of layer
-        activations = [x]
+        #activations = [x]
+        activations = [X]
         zs = []
 
         #Activate all layers
         for bias, weight in zip(self.biases, self.weights):
-            z = np.dot(weight,activation)+bias
-            np.append(zs,z)
+            z = np.dot(weight, activation)+bias
+            zs.append(z)
             activation = sigmoid(z)
-            np.append(activations,activation)
+            #print("Activations Back",activation)
+            #np.append(activations,activation)
+            activations.append(activation)
 
         """Now we pass backward using the chain rule to find the respective derivatives
         
@@ -280,99 +337,170 @@ class NeuralNetwork:
         Reach end of array with [-1]
         
         """
+
         delta = self.cost.delta(zs[-1],activations[-1],y)
-        der_w[-1] =
-        der_b[-1] =
+
+        der_w[-1] = np.dot(delta,activations[-2].T)
+        der_b[-1] = delta
+
+        #Start with next to out-layer
+        for l in range(2,self.n_layers):
+            z = zs[-l]
+            #print("Z",z.shape)
+            sig_der = sigmoid_der(z)
+            #print("Z-der", sigmoid_der(z).shape)
+            #For layer h
+            #delta_h = dC/d_ah X da_h/dz_h
+            # 1. dC/da_h = delta_(h+1)*w_h+1
+            # 2. da_h/dz_h = sigma'(z_h)
+            delta = np.dot(self.weights[-l+1].T,delta)*sig_der
+            #print("Activations",activations[-l-1].T)
+            #print("Weights",self.weights[-l+1].T.shape)
+            #print("Activations",activations[0])
+            der_w[-l] = np.dot(delta,activations[-l-1].T)
+            der_b[-l] = delta
+            #print("Derivate",der_w)
+
+        return der_b, der_w
+
+        # Se nærmere på hvordan X er ift vector
+    def SGD(self, X, y, epochs, m_batch_size, eta=0.1):
+        n = len(X[:])
+
+        for epoch in range(epochs):
+            # Pass på axis
+            randomize = np.arange(len(X[:,0]))
+
+            np.random.shuffle(randomize)
+
+            x_rand = X[randomize]
+            y_rand = y[randomize]
+            #print("batch size",m_batch_size)
+            mini_batches_x = [x_rand[m:m + m_batch_size] for m in range(0, n, m_batch_size)]
+            mini_batches_y = [y_rand[m:m + m_batch_size] for m in range(0, n, m_batch_size)]
+            k = 0
+
+            for batchx, batchy in zip(mini_batches_x, mini_batches_y):
+                #print("Batch no", k+1)
+
+                self.update_m_batch(batchx, batchy, epoch, eta)
+                k += 1
 
 
-        for h in range(2,self.n_layers):
+    def learning_schedule(self, t):
+        t0, t1 = 5, 50
+        return t0 / (t + t1)
+
+    #Update with regularization l2,l1
+    def update_m_batch(self, batchx, batchy, epoch, eta_i, m=100, i=1,lamd=0):
+        #gradients = self.gd(batchx, batchy, self.weights)
+
+        der_b = [np.zeros(bias.shape) for bias in self.biases]
+        der_w = [np.zeros(weight.shape) for weight in self.weights]
+
+
+        if eta_i == 0:
+            eta = self.learning_schedule((epoch * m + i))
+        eta = eta_i
+        #print("SGD eta", eta)
+        #print("M",m)
+        #self.weights -= eta * gradients
+
+        #print("eta_out",eta)
+
+        for x,y in zip(batchx,batchy):
+            #Get dCx/dw & dCx/db
+
+            delta_der_b, delta_der_w = self.backpropagate(x, y)
+            # Calculate SUM_x(dCx/dw) & SUM_x(dCx/db)
+            der_b = [db + d_db for db,d_db in zip(der_b,delta_der_b)]
+            der_w = [dw + d_dw for dw, d_dw in zip(der_w, delta_der_w)]
+
+        self.biases = [b-(eta/m)*db for b,db in zip(self.biases,der_b)]
+        self.weights = [(1-(eta*lamd/m))*w-(eta/m)*dw for w,dw in zip(self.weights,der_w)]
 
 
 
 
+    def feed_forward_out(self,X):
+        #Return activiation with a as input
+        a = 0
+        z_h = []
+        probabilities = np.zeros(len(X[:]))
+        for i in range(len(X[:])):
+            X_in = X[i,:]
+            X_in.reshape(-1,1)
+            """
+            for bias, weight in zip(self.biases, self.weights):
+                #print("Sample",activation)
+                z_h = sigmoid(np.dot(weight, activation)+bias)
+                activation = z_h
+            a_h = z_h.T
+            """
+            a_h = self.feed_forward(X_in).T
+            a = np.dot(self.weights[-1],a_h)+self.biases[-1]
+            probabilities[i] = sigmoid(a)
+            #probabilities[i] = a
+
+            #print(probabilities[i])
+
+        return probabilities
+
+
+    def predict(self, X,classify=False):
+        prob = self.feed_forward_out(X)
+        classification = prob
+        if classify:
+            for i in range(len(prob)):
+                if prob[i] >= 0.5:
+                    classification[i] = 1
+                    #print(True)
+                else:
+                    classification[i] = 0
+                    #print(False)
+        return classification
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+"""
+--------------------------------------------------------------------
+Init tester
+--------------------------------------------------------------------
+"""
 
 
 #Create unit test for functions file
 if __name__ == "__main__":
 
-    print("test")
-    """
-    X, y = generate_data()
-    X = np.c_[np.ones((X.shape[0], 1)), X]
-    y = y[:, np.newaxis]
-    """
-    """
-    data = load_data("marks.csv", None)
-    # X = feature values, all the columns except the last column
-    X = data.iloc[:, :-1]
-    # y = target values, last column of the data frame
-    y = data.iloc[:, -1]
-    # filter out the applicants that got admitted
-    admitted = data.loc[y == 1]
-    # filter out the applicants that din't get admission
-    not_admitted = data.loc[y == 0]
-    # plots
-    plt.scatter(admitted.iloc[:, 0], admitted.iloc[:, 1], s=10, label='Admitted')
-    plt.scatter(not_admitted.iloc[:, 0], not_admitted.iloc[:, 1], s=10, label='Not Admitted')
-
-    # preparing the data for building the model
-
-    X = np.c_[np.ones((X.shape[0], 1)), X]
-    y = y[:, np.newaxis]
-    """
-    """
-    num_observations = 5000
-
+    num_observations = 1000
     x1 = np.random.multivariate_normal([0, 0], [[1, .75],[.75, 1]], num_observations)
     x2 = np.random.multivariate_normal([1, 4], [[1, .75],[.75, 1]], num_observations)
-
     X = np.vstack((x1, x2)).astype(np.float32)
     y= np.hstack((np.zeros(num_observations),np.ones(num_observations)))
-
     X = np.hstack((np.ones((X.shape[0], 1)), X))
-    print("X",len(X[:]))
-    y = y.ravel()
-    n = len((X[:,1]))
-    #betas = np.zeros((X.shape[1],1))
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2)
+
+
     logreg = logit(X,y)
-    #betas = logreg.rnd_betas(n)
-    #logreg.cost_func(X , y,betas)
-    #grad = logreg.gd(X,y,betas)
-    #print("Beta_vec", betas.shape)
-    #print("Gamma", logreg.gd_fit(X,y,iter='NR').shape)
-    #print("Gradient", grad.shape)
-    #gamma = logreg.gd_fit(X,y,iter='NR')
-    #step = gamma@grad
     #print("Beta_vec",betas.shape)
-    print("Betas", logreg.gd_fit(X,y,iter='SGD'))
-    #print("Gradient",grad.shape)
-
-    clf = LogisticRegression(fit_intercept=True, C=1e15)
-    clf.fit(X[:,1:], y.ravel())
-
-    print("SKlearn results",clf.intercept_, clf.coef_)
+    #print("Betas", logreg.gd_fit(X,y,iter='NR'))
+    #clf = LogisticRegression(fit_intercept=True, C=1e15)
+    #clf.fit(X[:,1:], y.ravel())
+    #print("SKlearn results",clf.intercept_, clf.coef_)
 
 
-    #print(step.shape)
-    """
 
+    sizes = [3,50,1]
+    etas = np.logspace(-5, -1, 20)
+    lamb = np.logspace(-5, -1, 7)
+    #Run test funtion
+    grid_search(NeuralNetwork,sizes,X_test,y_test,etas,lamb)
 
+    sns.set()
+    test_accuracy = np.zeros((len(etas), len(lamb)))
+
+"""
 sizes = [2,3,1]
 
 
@@ -384,4 +512,5 @@ for x, y in zip(sizes[:-1], sizes[1:]):
     weights = np.random.randn(y,x)
     print(weights)
 
+"""
 
